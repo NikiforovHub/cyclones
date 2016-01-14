838390e34e9dd6668a969ad8c21720c40bdae6e0
NikiforovHub <box_nikiforov_sergey@mail.ru>
2016-01-14 05:02:25 +0300

    Minor changes in find_contour function
    ggmap_map_frame and leaflet_map_frame were deleted because of them
    duplicate same functions in plot.r

--- find_cyclones_data.R
+++ find_cyclones_data.R
@@ -416,14 +416,16 @@ find_contour = function(contours, center_lon_ind, check_lat_ind){
   if (l){
     contour = NULL
     for (i in 1:l){
-      contours[[i]]$x = sapply(contours[[i]]$x,round)
-      contours[[i]]$y = sapply(contours[[i]]$y,round)
-      bool = contours[[i]]$x == center_lon_ind
+      #       contours[[i]]$x = sapply(contours[[i]]$x,round)
+      #       contours[[i]]$y = sapply(contours[[i]]$y,round)
+      bool = ((contours[[i]]$x < center_lon_ind + 1) & 
+                (contours[[i]]$x > center_lon_ind - 1))
       lat_ind_vector = contours[[i]]$y[bool]
       m = length(lat_ind_vector)
       if (m){
         for (k in 1:m){
-          if (lat_ind_vector[k] == check_lat_ind){
+          if ((lat_ind_vector[k] < check_lat_ind + 1) &
+              (lat_ind_vector[k] > check_lat_ind - 1)){
             level = list(level = contours[[i]]$level)
             x = list(x = contours[[i]]$x)
             y = list(y = contours[[i]]$y)
